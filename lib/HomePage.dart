52e1d557fe52c9b5a61a3e1de20d02f3b4c6b5a9
jiyoung <boytoboy0108@gmail.com>
2022-06-28 00:52:29 +0900

    Image class is partially applied in HomePage.dart. --> still have sync issue. --> need to figure out a way to handle filesAll and Dates to get it imediately without pressing button. all images can be displayed in HomePage.dart as button.

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -14,13 +14,18 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   var images = Images();
-  var files;
+  var files = null;
   var dates;
 
 
   void initState() {
-    getFiles(); //call getFiles() function on initial state.
+    // getFiles(); //call getFiles() function on initial state.
     super.initState();
+    setState((){
+      files = images.filesAll;
+      dates = images.dates;});
+
+    print("files : $files");
   }
 
   Widget Button(String text) {
@@ -32,7 +37,7 @@ class _HomePageState extends State<HomePage> {
       decoration: BoxDecoration(
         color: Colors.blue,
         shape: BoxShape.rectangle,
-        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+        borderRadius: BorderRadius.all(Radius.circular(10.0)),
       ),
       child: FlatButton(
         padding: EdgeInsets.all(5),
@@ -45,79 +50,99 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  void getFiles() async {
-    //asyn function to get list of files
-    // List<StorageInfo> storageInfo = await PathProvider.getStorageInfo();
-    // var root = storageInfo[0].rootDir; //storageInfo[1] for SD card, geting the root directory
-
-    var status = await Permission.storage.status;
-    if (!status.isGranted) {
-      await Permission.storage.request();
-    }
-    var root = '/sdcard/DCIM/Camera';
-    var fm = FileManager(root: Directory(root)); //
-    files = await fm.filesTree(
-        extensions: [
-          "png",
-          "jpg"
-        ] //optional, to filter files, remove to list all,
-        );
-
-    files = files.where((item) => item.toString().contains('2022')).toList();
-    var dateList = [];
-    for (var i = 0; i < files.length; i++) {
-      dateList.add(files[i].toString().split('/')[4].substring(0, 8));
-    }
-    dates = dateList.toSet().toList();
-    setState(() {}); //update the UI
-  }
-
-  void getDateFromFiles() async {
-    var dateList = [];
-    print('files_all = $files.length');
-    for (var i = 0; i < files.length; i++) {
-      dateList.add(files[i].toString().split('/')[4].substring(0, 8));
-    }
-    dates = dateList.toSet().toList();
-    print(dates);
-    setState(() {}); //update the UI
-  }
+  // void getFiles() async {
+  //   //asyn function to get list of files
+  //   // List<StorageInfo> storageInfo = await PathProvider.getStorageInfo();
+  //   // var root = storageInfo[0].rootDir; //storageInfo[1] for SD card, geting the root directory
+  //
+  //   var status = await Permission.storage.status;
+  //   if (!status.isGranted) {
+  //     await Permission.storage.request();
+  //   }
+  //   var root = '/sdcard/DCIM/Camera';
+  //   var fm = FileManager(root: Directory(root)); //
+  //   files = await fm.filesTree(
+  //       extensions: [
+  //         "png",
+  //         "jpg"
+  //       ]
+  //       );
+  //
+  //   files = files.where((item) => item.toString().contains('2022')).toList();
+  //   var dateList = [];
+  //   for (var i = 0; i < files.length; i++) {
+  //     dateList.add(files[i].toString().split('/')[4].substring(0, 8));
+  //   }
+  //   dates = dateList.toSet().toList();
+  //   setState(() {}); //update the UI
+  // }
+  //
+  // void getDateFromFiles() async {
+  //   var dateList = [];
+  //   print('files_all = $files.length');
+  //   for (var i = 0; i < files.length; i++) {
+  //     dateList.add(files[i].toString().split('/')[4].substring(0, 8));
+  //   }
+  //   dates = dateList.toSet().toList();
+  //   print(dates);
+  //   setState(() {}); //update the UI
+  // }
 
   Widget build(BuildContext context) {
     return Scaffold(
       body: files == null
           ? Text("Searching Files")
           : SafeArea(
-              child: Column(children: [
-                Row(
-                  children: [
-                    Button(dates[0]),
-                    Button(dates[1]),
-                    Button(dates[2]),
-                    Button(dates[3]),
-                    Button(dates[4]),
-                  ],
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                ),
-                SizedBox(height: 5),
-                Row(
-                  children: [
-                    Button(dates[5]),
-                    Button(dates[6]),
-                    Button(dates[7]),
-                    Button(dates[8]),
-                    Button(dates[9]),
-                  ],
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                )
-              ]),
-            ),
+            minimum: EdgeInsets.all(40),
+             child: GridView.builder(
+               physics : NeverScrollableScrollPhysics(),
+               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                 crossAxisCount: 5,
+                 crossAxisSpacing: 20,
+                 mainAxisSpacing: 20,
+               ),
+
+               itemCount: dates.length,
+               itemBuilder: (context, index){
+                 return Button(
+                   dates[index]
+                 );}),
+
+               ),
+
+
+        // child: Column(children: [
+        //         Row(
+        //           children: [
+        //             // Button(dates[0]),
+        //             // Button(dates[1]),
+        //             // Button(dates[2]),
+        //             // Button(dates[3]),
+        //             // Button(dates[4]),
+        //           ],
+        //           mainAxisSize: MainAxisSize.max,
+        //           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //           crossAxisAlignment: CrossAxisAlignment.end,
+        //         ),
+        //         SizedBox(height: 5),
+        //         Row(
+        //           children: [
+        //             // Button(dates[5]),
+        //             // Button(dates[6]),
+        //             // Button(dates[7]),
+        //             // Button(dates[8]),
+        //             // Button(dates[9]),
+        //           ],
+        //           mainAxisSize: MainAxisSize.max,
+        //           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //           crossAxisAlignment: CrossAxisAlignment.end,
+        //         )
+        //       ]),
+        //     ),
       floatingActionButton: FloatingActionButton(onPressed: () {
         images.updateState();
+        setState((){dates = images.dates;});
+        print(dates);
       }),
     );
   }
--- lib/Images.dart
+++ lib/Images.dart
@@ -1,34 +1,28 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_file_manager/flutter_file_manager.dart';
 import 'dart:io';
 import 'package:ml_dataframe/ml_dataframe.dart';
 
-
-
 // TODO : sort image. by year, by month, by date
 // TODO : get all files, --> filesAll
 //TODO : sort filesSortByYear, filesSortByMonth, filesSortByDate
 
-
 //TODO : class - get files, def so
 
-
-class Images{
+class Images {
   List<File> filesAll = [];
   List<String> years = [];
   List<String> months = [];
   List<String> dates = [];
 
-  Images(){
+  Images() {
     getFiles();
     years = getYearFromFiles();
     months = getMonthFromFiles();
     dates = getDateFromFiles();
-
   }
 
-  void updateState(){
+  void updateState() {
     getFiles();
     years = getYearFromFiles();
     months = getMonthFromFiles();
@@ -36,50 +30,57 @@ class Images{
     print('$years, $months, $dates');
   }
 
+  // List<File> getFilesAll() {return filesAll;}
+  // List<String> getYears() {return years;}
+  // List<String> getMonths() {return years;}
+  // List<String> getDates() {return years;}
+
   void getFiles() async {
     //asyn function to get list of files
     // List<StorageInfo> storageInfo = await PathProvider.getStorageInfo();
     // var root = storageInfo[0].rootDir; //storageInfo[1] for SD card, geting the root directory
     var root = '/sdcard/DCIM/Camera';
     var fm = FileManager(root: Directory(root)); //
-    filesAll = await fm.filesTree(
-        extensions: [
-          "png",
-          "jpg"
-        ] //optional, to filter files, remove to list all,
-    );
+    filesAll = await fm.filesTree(extensions: [
+      "png",
+      "jpg"
+    ] //optional, to filter files, remove to list all,
+        );
+
   }
 
   List<File> sortFilesByDate(String date) {
-    var filesSortByDate ;
-    filesSortByDate = filesAll.where((item) => item.toString().contains('$date')).toList();
+    var filesSortByDate;
+    filesSortByDate =
+        filesAll.where((item) => item.toString().contains('$date')).toList();
 
     return filesSortByDate;
   }
 
-  List<String> getYearFromFiles(){
+  List<String> getYearFromFiles() {
     List<String> yearFromFilename = [];
-    for(int i =0; i < filesAll.length; i++){
-      yearFromFilename.add(filesAll[i].toString().split('/')[4].substring(0, 4));
+    for (int i = 0; i < filesAll.length; i++) {
+      yearFromFilename
+          .add(filesAll[i].toString().split('/')[4].substring(0, 4));
     }
     return yearFromFilename.toSet().toList();
   }
 
-  List<String> getMonthFromFiles(){
+  List<String> getMonthFromFiles() {
     List<String> monthFromFilename = [];
-    for(int i =0; i < filesAll.length; i++){
-      monthFromFilename.add(filesAll[i].toString().split('/')[4].substring(0, 6));
+    for (int i = 0; i < filesAll.length; i++) {
+      monthFromFilename
+          .add(filesAll[i].toString().split('/')[4].substring(0, 6));
     }
     return monthFromFilename.toSet().toList();
   }
 
-  List<String> getDateFromFiles(){
+  List<String> getDateFromFiles() {
     List<String> dateFromFilename = [];
-    for(int i =0; i < filesAll.length; i++){
-      dateFromFilename.add(filesAll[i].toString().split('/')[4].substring(0, 8));
+    for (int i = 0; i < filesAll.length; i++) {
+      dateFromFilename
+          .add(filesAll[i].toString().split('/')[4].substring(0, 8));
     }
     return dateFromFilename.toSet().toList();
   }
-
-
 }
